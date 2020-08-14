1b8956769a5a04b3e891171cef10546414167116
GaVaACJA <gvalverde99@gmail.com>
2020-08-14 08:26:53 -0300

    v9

--- js/login.js
+++ js/login.js
@@ -14,8 +14,14 @@ function loginOk() { //creamos un objeto que indique que el proceso fue correcto
    document.location.href = "https://gabriel-jap.github.io/index.html";
 }
 
-function onSignIn(googleUser){
+function onSignIn(){
    var profile = googleUser.getBasicProfile();
-   console.log(profile);
-   loginOk();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        loginOk();
 }
