fde6b95c38eae62003c89749b1212815274d4db6
GaVaACJA <gvalverde99@gmail.com>
2020-08-15 19:11:19 -0300

    entrega 1

--- js/init.js
+++ js/init.js
@@ -40,25 +40,29 @@ var getJSONData = function(url){
     });
 }
 
-document.addEventListener("DOMContentLoaded", function (e) {
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
   const salida = document.getElementById('logoutButton');
   salida.onclick = () => {
-      localStorage.clear();
-      console.log("logout")
-      document.location.href = "login.html";
-  }
+    localStorage.clear();
+    console.log("logout")
+    document.location.href = "login.html";}
+  
 
-})
 
-window.onload = () => {
-  let token = localStorage.getItem('token');
+  window.onload = () => {
+  let token =localStorage.getItem('token');
   let google = localStorage.getItem('oauth2_ss::https://gabriel-jap.github.io::1::DEFAULT::_ss_')
-  if (token === null) {
+  if (token===null) {
       console.log("no hay token")
       document.location.href = "login.html";
-  } else {
+  }else{
       console.log("estoy en inicio")
   }
 }
+
+})
 
 
