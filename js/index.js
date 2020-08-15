646406d68c69acef79fd82458ac77c46c5ab64c6
GaVaACJA <gvalverde99@gmail.com>
2020-08-15 18:27:27 -0300

    prueba11

--- js/index.js
+++ js/index.js
@@ -4,9 +4,17 @@ document.addEventListener("DOMContentLoaded", function (e) {
     const salida = document.getElementById('logoutButton');
     salida.onclick = () => {
         localStorage.clear();
+        console.log("logout")
+        document.location.href = "login.html";
     }
 
-    // window.onload = () => {
+     
+
+    
+
+})
+
+window.onload = () => {
         let token =localStorage.getItem('token');
         let google = localStorage.getItem('oauth2_ss::https://gabriel-jap.github.io::1::DEFAULT::_ss_')
         if (token===null) {
@@ -15,8 +23,4 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }else{
             console.log("estoy en inicio")
         }
-    //}
-
-    
-
-})
+    }
