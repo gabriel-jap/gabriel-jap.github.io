0473f82ef32378f9679b9da016f442a21cb1b593
GaVaACJA <gvalverde99@gmail.com>
2020-08-14 22:11:27 -0300

    Sera

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
-window.onload = () => {
-    let token =localStorage.getItem('token');
-    if (!token) {
-        document.location.href = "login.html";
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    const salida = document.getElementById('logoutButton');
+    salida.onclick = () => {
+        localStorage.clear();
     }
-}
+
+    window.onload = () => {
+        let token =localStorage.getItem('token');
+        console.log("Reciví "+token )
+        if (!token) {
+            console.log("hmm")
+            document.location.href = "login.html";
+        }else{
+            console.log("no")
+        }
+    }
+
+    
+
+})
