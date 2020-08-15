563c3e42575a3e5dd07c5587275fe796ca58cab7
GaVaACJA <gvalverde99@gmail.com>
2020-08-15 18:38:22 -0300

    prueba12

--- js/init.js
+++ js/init.js
@@ -43,13 +43,26 @@ var getJSONData = function(url){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){const logoutButton = document.getElementById('logout');
-let token = localStorage.getItem('token');
-logoutButton.onclick = () => {
-localStorage.removeItem('token');
-window.location.href = '/login';
-};
-if(!token) {
+document.addEventListener("DOMContentLoaded", function(e){
+  const salida = document.getElementById('logoutButton');
+  salida.onclick = () => {
+    localStorage.clear();
+    console.log("logout")
+    document.location.href = "login.html";}
+  
+
+
+  window.onload = () => {
+  let token =localStorage.getItem('token');
+  let google = localStorage.getItem('oauth2_ss::https://gabriel-jap.github.io::1::DEFAULT::_ss_')
+  if (token===null) {
+      console.log("no hay token")
+      document.location.href = "login.html";
+  }else{
+      console.log("estoy en inicio")
+  }
 }
-});
-window.location.href = '/login';
+
+})
+
+
