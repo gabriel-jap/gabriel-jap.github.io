bc791d86bd159b02e2bba79fa14b0b7c43585bb3
GaVaACJA <gvalverde99@gmail.com>
2020-09-11 15:24:20 -0300

    Entrega 3 (resuelto error de coment)

--- js/product-info.js
+++ js/product-info.js
@@ -38,70 +38,68 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }
     });
 
-    showComments()
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            obj = resultObj.data;
+            showComments()
+        }
+    });
+
 
     form.onsubmit = (e) => {
         e.preventDefault()
         let score
-        let texto
+        let texto = formText.value
         let date = new Date();
         let mes = cero(date.getMonth() + 1);
         let dia = cero(date.getDay());
-        let fecha = date.getFullYear() + "-" + mes + "-"+dia+" " + date.toLocaleTimeString()
-
-        texto = formText.value
+        let fecha = date.getFullYear() + "-" + mes + "-" + dia + " " + date.toLocaleTimeString() // fecha con formato
 
         for (var i = 0, length = radios.length; i < length; i++) {
             if (radios[i].checked) {
-                // do whatever you want with the checked radio
+                // obtiene valor del radio seleccionado
                 score = radios[i].value
 
-                // only one radio can be logically checked, don't check the rest
+                // solo habrá un radio selecionado
                 break;
             }
         }
-        showComments({"score": score,
-        "description": texto,
-        "user": localStorage.getItem("user"),
-        "dateTime": fecha});
-
-
-
+        //creo la estructura del comentario y la envio a la función
+        showComments({
+            "score": score,
+            "description": texto,
+            "user": localStorage.getItem("user"),
+            "dateTime": fecha
+        });
     }
 });
 
-
 function showComments(newComent = null) {
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            obj = resultObj.data;
-            if (!(newComent===null)) {
-                obj.push(newComent)
-            }
-            obj.forEach(coment => {
-                comentarios.innerHTML += '<div class=list-group-item><b>' + coment.user + '</b><span class="star">' + stars(coment.score) + '</span><p class="text-muted">' + coment.dateTime + '</p><p>"' + coment.description + '"</p></div>'
-            })
-
-        }
-    });
+    if (!(newComent === null)) {//Si el parametro esta definido, agrega un comentario
+        obj.push(newComent)
+    }
+    comentarios.innerHTML = "";//limpia los antiguos comentarios
+    obj.forEach(coment => {
+        comentarios.innerHTML += '<div class=list-group-item><b>' + coment.user + '</b><span class="star">' + stars(coment.score) + '</span><p class="text-muted">' + coment.dateTime + '</p><p>"' + coment.description + '"</p></div>'
+    })//Dibuja todos los comentarios
 }
 
 function stars(score) {
     let html = "";
     for (let index = 0; index < 5; index++) {
-        if (score > index) {
+        if (score > index) { // Genera tantas estrllas como puntuacion tenga el articulo, luego rellena con estrellas vaias hasta llegar a 5
             html += '<span class="fa fa-star checked"></span>'
         } else {
             html += '<span class="fa fa-star"></span>'
         }
     }
-    return html;
+    return html;//Devuelve el codigo para generar
 
 }
 
-function cero(i) {
-            if (i < 10) {
-                i = '0' + i;
-            }
-            return i;
-        }
+function cero(i) { //(Solo por formato) cambia la fecha en caso de que el mes o el día al formato mm o dd
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
+}
