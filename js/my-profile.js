ae56b8badbcbbb026ec747ee3eac714dd8e6fb7e
GaVaACJA <gvalverde99@gmail.com>
2020-11-04 09:44:32 -0300

    Entrega7

--- js/my-profile.js
+++ js/my-profile.js
@@ -5,8 +5,26 @@ const inputEmail = document.getElementById("eMailForm")
 const inputTel = document.getElementById("telForm")
 const inputNewPic = document.getElementById("photoForm")
 const userPic = document.getElementById("photoUser")
-const save = document.getElementById("btnSave")
+const formUser = document.getElementById("userChange")
 const pastuser = localStorage.getItem("datos")
+var newImg = ""
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      
+      reader.onload = function(e) { // donde ocurre la magia
+
+        userPic.src=e.target.result;
+        newImg=e.target.result;
+      }
+      reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+  }
+
+  inputNewPic.addEventListener("change",function(){
+      readURL(this)
+  })
 
 document.addEventListener("DOMContentLoaded", function (e) {
     let user = JSON.parse(localStorage.getItem("datos"))
@@ -15,19 +33,20 @@ document.addEventListener("DOMContentLoaded", function (e) {
     inputAge.value = user.age
     inputEmail.value = user.email
     inputTel.value = user.tel
+    userPic.src=user.pic
 });
 
-save.addEventListener("click", e =>{
-    //Funciona
-    userPic.setAttribute("src", "https://i.ibb.co/1GQWg5Z/gato.png")
+formUser.addEventListener("submit", function (e) {
+    e.preventDefault();
     let datos = {
         name: inputUsername.value,
         surname: inputSurname.value,
         age: inputAge.value,
         email: inputEmail.value,
         tel: inputTel.value,
-        pic: inputNewPic.value,
-    }
-    localStorage.setItem("datos", JSON.stringify(datos))
-    location.reload()
+        pic: newImg,
+    };
+    localStorage.setItem("datos", JSON.stringify(datos));
 })
+
+
